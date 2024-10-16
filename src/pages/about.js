f8f8aa404db0b61a3bb6fd70f1317a7dfc09b0ca
Nico Dann <nicodann@gmail.com>
2024-10-15 20:44:39 -0400

    Create about page.

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>My site my rulez.</p>
+    </main>
+  )
+}
+
+export const Head = () => (
+  <>
+    <title>About Nico Dann</title>
+    <meta name="description" content="About Nico Dann web developer"/>
+  </>
+)
+
+export default AboutPage
